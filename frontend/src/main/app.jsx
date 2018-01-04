c56f680225f101296932f3a3649d8e5a56dd5c10
Santos, Rodrigo dos A <rodrigo.dos.a.santos@accenture.com>
2018-01-04 16:14:21 -0200

    Initial commit

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,13 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+
+export default props => (
+    <div className="container">
+       <div className="jumbotron">
+        <h1>Learn React!</h1>
+        <p><a className="btn btn-primary btn-lg" href="#" role="button">Button</a></p>
+        </div>
+    </div>
+)
